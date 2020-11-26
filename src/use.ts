308639d26148c1d00fda6808f62af2f402e7236b
Sangwoo <swp@laonpeople.com>
2020-11-26 09:46:29 +0900

    Create use decorator

---
+++ src/use.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata';
+import { RequestHandler } from "express";
+
+import { MetaKeys } from "./MetaKeys";
+
+/**
+ * Use decorator function factory.
+ * The "Use" decorator adds middleware to the routes.
+ *
+ * @param middleware Middleware to add.
+ */
+export function Use(middleware: RequestHandler) {
+    return function(target: any, key: string, desc: PropertyDescriptor) {
+        const middlewares = Reflect.getMetadata(MetaKeys.Middleware, target, key) || [];
+
+        middlewares.push(middleware);
+
+        Reflect.defineMetadata(MetaKeys.Middleware, [...middlewares], target, key);
+    }
+}

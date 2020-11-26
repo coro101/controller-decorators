b4f4efec4a44f4b01aeda8edf0e5dffca9acb3db
Sangwoo <swp@laonpeople.com>
2020-11-26 09:41:02 +0900

    Create singleton AppRouter

---
+++ src/utils/AppRouter.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+
+/**
+ * Simple singleton class that returns the unique router object.
+ */
+export default class AppRouter {
+    private static instance: express.Router;
+
+    private constructor() {}
+
+    /**
+     * Return the instance.
+     *
+     * @returns Instance of the unique router instance.
+     */
+    static getInstance(): express.Router {
+        if (!AppRouter.instance) {
+            AppRouter.instance = express.Router();
+        }
+
+        return AppRouter.instance;
+    }
+}
